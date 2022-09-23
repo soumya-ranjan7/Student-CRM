509261c3a578e5febf92015946951f8e32e75f8a
soumya-ranjan7 <soumya.cr17@gmail.com>
2022-09-23 19:02:54 +0530

    Create Project

---
+++ src/main/java/com/example/studentcrm/StudentCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentcrm/StudentCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
